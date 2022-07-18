8fbcff5fc9821251402148cb8c23ca758de4d941
Marty <martynolan17@gmail.com>
2022-07-18 15:47:00 -0400

    weather data appends to the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,45 @@
 searchButtonEl = document.querySelector(".searchBtn");
 
+var temp = document.querySelector(".temp");
+var cityNameDate = document.querySelector(".cityNameDate")
+var currentDate = moment().format("dddd, MM/DD/YY");
+var humidity = document.querySelector(".humidity");
+var wind = document.querySelector(".wind");
+var uvIndex = document.querySelector(".uv-index");
+
 const searchCity = (cityName) =>{
     cityName.preventDefault();
     let city = document.getElementById("user-input").value;
     var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=";
-    var apiKey = "&units=imperial&appid=55494ace23520784606e14e197b5fad1";
+    var apiKey = "&units=metric&appid=55494ace23520784606e14e197b5fad1";
     var apiFetch = weatherApi + city + apiKey;
         fetch(apiFetch)
         .then(response => response.json())
         .then(data => {
+            const name = data.name
             const tempValue = data.main.temp
-            const humid = data.main.humidity
+            const humidityValue = data.main.humidity
             const windValue = data.wind.speed
+            console.log(currentDate);
+            console.log(name);
             console.log(tempValue);
-            console.log(humid);
+            console.log(humidityValue);
             console.log(windValue);
+            // console.log(lon);
+            // console.log(lat);
+
+            cityNameDate.innerHTML = "";
+            cityNameDate.innerHTML = `${name} (${currentDate})`;
+            temp.innerHTML = `Temp: ${tempValue}&#176C`;
+            wind.innerHTML = `Wind: ${windValue} km/h`;
+            humidity.innerHTML = `Humidity: ${humidityValue}%`;
+            // uvIndex.innerHTML = `UV Index: ${uvIndexValue}`;
+
         })
 }
 
 document.getElementById("searchBtn").addEventListener("click", searchCity);
+
+
+
+
